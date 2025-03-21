131804fc899e9c23440002cb9317fc751d2d1e37
carlos <carloskip25@gmail.com>
2025-03-21 08:02:14 +0300

    second assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+//const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+
+//const welcomeMessage = currentUser;
+
+//const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+
+//typeof "const currentUser = 'Grace Hopper';";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting = '';
+//const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser}!`;
